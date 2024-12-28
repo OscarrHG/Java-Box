65837e801530f72a86e6b25d947d2fa68d97fdfc
OscarrHG <hgoscar3@gmail.com>
2024-12-27 22:43:17 -0500

    Math

---
+++ src/Clase_Math.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Clase_Math {
+    public static void main(String[] args) {
+        //Calcular la raiz cuadrada
+//        int valor = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un valor: "));
+//        JOptionPane.showMessageDialog(null, "Raiz: " + Math.sqrt(valor));
+
+        //Potencia de un numero
+        var base = 2;
+        var exponente = 3;
+        JOptionPane.showMessageDialog(null, "Potencia de 2^3 es: " + Math.pow(base,exponente));
+
+        // Valor absoluto de un número
+        var absoluto = -2.5;
+        System.out.println("Valor absoluto de " + absoluto + " = " + Math.abs(absoluto));
+
+        // Identificar mayor valor
+        var numero1 = 50.2;
+        var numero2 = 300;
+        System.out.println("Mayor valor: " + Math.max(numero2, numero1));
+
+        //Redondear
+        var  total = 2.345;
+    }
+}
