5e90b9f6f3ec5e8c9484f3ff647700842d10cf68
OscarrHG <hgoscar3@gmail.com>
2024-12-31 15:55:26 -0500

    Arrays Clas

---
+++ src/Arreglos.java
@@ -0,0 +1,20 @@
+public class Arreglos {
+    public static void main(String[] args) {
+        //Declaracion de un Array
+        int[] numeros = new int[5];
+
+        //Inicializacion de un Array - Forma 1
+        numeros[0] = 50;
+        numeros[1] = 20;
+        numeros[2] = 35;
+        numeros[3] = 80;
+        numeros[4] = 150;
+
+        //Inicializacion de un Array - Forma 2
+        int[] valores = {234, 345, 456};
+
+        //Consultar indices del array
+        System.out.println(numeros[2]);
+        System.out.println(valores[0]);
+    }
+}
---
+++ src/Arreglos_Caracteres.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Arreglos_Caracteres {
+    public static void main(String[] args) {
+        String[] productos = new String[5];
+
+        productos[0] = "Manazanas";
+        productos[1] = "Uvas";
+        productos[2] = "Peras";
+        productos[3] = "Fresas";
+        productos[4] = "Kiwis";
+
+        System.out.println("producto 1: " + productos[0]);
+        System.out.println("producto 2: " + productos[1]);
+        System.out.println("producto 3: " + productos[2]);
+        System.out.println("producto 4: " + productos[3]);
+        System.out.println("producto 5: " + productos[4]);
+
+        Arrays.sort(productos);
+
+        System.out.println("\nLista Ordenada\nproducto 1: " + productos[0]);
+        System.out.println("producto 2: " + productos[1]);
+        System.out.println("producto 3: " + productos[2]);
+        System.out.println("producto 4: " + productos[3]);
+        System.out.println("producto 5: " + productos[4]);
+    }
+}
