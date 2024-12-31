e43e32ff42e319450243c01f8b84f839dbe06e0e
OscarrHG <hgoscar3@gmail.com>
2024-12-30 21:35:44 -0500

    Cambio de Version JDK

--- src/Clase_Math.java
+++ src/Clase_Math.java
@@ -1,5 +1,3 @@
-import javax.swing.*;
-
 public class Clase_Math {
     public static void main(String[] args) {
         //Calcular la raiz cuadrada
@@ -7,20 +5,21 @@ public class Clase_Math {
 //        JOptionPane.showMessageDialog(null, "Raiz: " + Math.sqrt(valor));
 
         //Potencia de un numero
-        var base = 2;
-        var exponente = 3;
-        JOptionPane.showMessageDialog(null, "Potencia de 2^3 es: " + Math.pow(base,exponente));
-
-        // Valor absoluto de un número
-        var absoluto = -2.5;
-        System.out.println("Valor absoluto de " + absoluto + " = " + Math.abs(absoluto));
-
-        // Identificar mayor valor
-        var numero1 = 50.2;
-        var numero2 = 300;
-        System.out.println("Mayor valor: " + Math.max(numero2, numero1));
-
-        //Redondear 29/12
-        var  total = 2.345;
+//        var base = 2;
+//        var exponente = 3;
+//        JOptionPane.showMessageDialog(null, "Potencia de 2^3 es: " + Math.pow(base,exponente));
+//
+//        // Valor absoluto de un número
+//        var absoluto = -2.5;
+//        System.out.println("Valor absoluto de " + absoluto + " = " + Math.abs(absoluto));
+//
+//        // Identificar mayor valor
+//        var numero1 = 50.2;
+//        var numero2 = 300;
+//        System.out.println("Mayor valor: " + Math.max(numero2, numero1));
+//
+//        //Redondear 29/12
+//        var  total =
+        System.out.println("Versión de Java: " + System.getProperty("java.version"));
     }
 }
